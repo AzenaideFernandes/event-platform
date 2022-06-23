77d50b5707f33f315ba0b69e05ebdccf6129b845
AzenaideFernandes <azenaidefkt@gmail.com>
2022-06-23 11:59:50 -0400

    commit 05
    commit 05

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import { gql, useQuery} from "@apollo/client"
+import { ApolloProvider, gql, useQuery} from "@apollo/client"
+import { BrowserRouter } from "react-router-dom"
 import { Header } from "./components/Header"
 import { Lesson } from "./components/Lesson"
+import { Router } from "./components/Router"
 import { Sidebar } from "./components/Sidebar"
 import { Video } from "./components/Video"
+import { client } from "./lib/apollo"
 import { Event} from "./pages/Event"
 
 
 
 function App() {
-    return (     
-        <Event />        
+    return (  
+      <ApolloProvider client={client}>
+        <BrowserRouter>      
+        <Router /> 
+        </BrowserRouter>
+    </ApolloProvider>   
+              
   )
 }
 
